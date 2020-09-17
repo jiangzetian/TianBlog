ed82a18ac1ec935ee48d730cc739a286bcdf4dad
蒋泽天 <1139246375@qq.com>
2020-09-17 22:47:59 +0800

    修改文章页

--- pages/article.vue
+++ pages/article.vue
@@ -12,23 +12,33 @@
     </el-row>
     <el-row type="flex" justify="center" :gutter="20">
       <el-col :md="20" :xs="24">
-        <nuxt-link class="link" v-for="(o, index) in 6" :key="o" to="/">
-          <el-col :md="8" :sm="12" :xs="24">
+          <el-col :md="8" :sm="12" :xs="24" v-for="(o, index) in 6" :key="o">
             <el-card class="article-item" :body-style="{ padding: '0px' }">
-              <el-image
-                style="width:100%; height:200px"
-                src="https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg"
-                fit="cover"></el-image>
-              <div style="padding: 14px;">
-                <h2>无用户登录状态网站的购物车功能实现方案</h2>
-                <p>最近遇到一个项目，值得写篇文章进行分析探讨和深入学习交流，所以决定写这篇文章，连续爆肝一下午，写出了Demo，晚上在整理写文章。（非主流无用户登录状态的购物车功能解决方案，不带第三方支付功能）</p>
-                <div class="bottom clearfix">
-                  <time class="time">2020年08月14日</time>
+              <nuxt-link class="link" to="/">
+                <el-image
+                  style="width:100%; height:200px"
+                  src="https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg"
+                  fit="cover"></el-image>
+                <div class="cont">
+                  <h2>无用户登录状态网站的购物车功能实现方案</h2>
+                  <p>最近遇到一个项目，值得写篇文章进行分析探讨和深入学习交流，所以决定写这篇文章，连续爆肝一下午，写出了Demo，晚上在整理写文章。（非主流无用户登录状态的购物车功能解决方案，不带第三方支付功能）</p>
                 </div>
+              </nuxt-link>
+              <div class="bottom clearfix">
+                <time class="time">2020年08月14日</time>
+                <el-row>
+                  <el-col :span="5">
+                    <i class="el-icon-view"></i>
+                    <span>999</span>
+                  </el-col>
+                  <el-col :span="5">
+                    <i class="el-icon-thumb"></i>
+                    <span>999</span>
+                  </el-col>
+                </el-row>
               </div>
             </el-card>
           </el-col>
-        </nuxt-link>
       </el-col>
     </el-row>
     <el-row  type="flex" justify="center" class="pages">
@@ -89,16 +99,19 @@ export default {
   width: 100%;
   margin: 0 auto;
 }
-.article-item h2{
+.article-item .cont{
+  padding: 10px;
+}
+.article-item .cont h2{
   font-size: 22px;
-  margin-bottom: 30px;
+  margin-bottom: 15px;
   display: -webkit-box;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 2;
   overflow: hidden;
   color: #303133;
 }
-.article-item p{
+.article-item .cont p{
   font-size: 16px;
   display: -webkit-box;
   -webkit-box-orient: vertical;
@@ -107,10 +120,10 @@ export default {
   color: #606266;
 }
 .article-item .bottom{
-  margin-top: 20px;
-}
-.article-item .time{
   color: #909399;
+  padding: 10px;
+}
+.article-item .bottom .time{
   width: 100%;
   display: block;
   text-align: right;
