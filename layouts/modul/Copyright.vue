8b2bad87835ee8dd1563bb6758e63db1e66fae09
蒋泽天 <1139246375@qq.com>
2023-12-03 21:42:01 +0800

    feat：加入版权信息动态显示

--- layouts/modul/Copyright.vue
+++ layouts/modul/Copyright.vue
@@ -1,29 +1,42 @@
 <template>
   <div id="copyright">
     <ul>
-      <li>天小天版权所有©</li>
-      <li>
-        <a no-prefetch target="_blank" href="https://beian.miit.gov.cn">
-          湘ICP备18008856号
-        </a>
-      </li>
-      <li>
-        <a
-          no-prefetch
-          target="_blank"
-          href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=43011102001726"
-        >
-          <img src="@/static/image/layout/备案图标.png" />
-          <span>湘公网安备 43011102001726号</span>
-        </a>
-      </li>
+        <li>天小天版权所有©</li>
+        <template v-if="host.includes('tianxiaotian')">
+            <li>
+                <a no-prefetch target="_blank" href="https://beian.miit.gov.cn">
+                    湘ICP备18008856号
+                </a>
+            </li>
+            <li>
+              <a
+                no-prefetch
+                target="_blank"
+                href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=43011102001726"
+              >
+                <img src="@/static/image/layout/备案图标.png" />
+                <span>湘公网安备 43011102001726号</span>
+              </a>
+            </li>
+        </template>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Copyright"
+  name: "Copyright",
+    data(){
+        return {
+            host:''
+        }
+    },
+   
+    mounted() {
+       if (process.browser) {
+           this.host = location.host
+       }
+    },
 };
 </script>
 
