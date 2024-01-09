863e6b3884c4793e8a6bce541a74eb49221f8660
蒋泽天 <1139246375@qq.com>
2024-01-09 09:38:53 +0800

    备案信息

--- layouts/modul/Copyright.vue
+++ layouts/modul/Copyright.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="copyright">
     <ul>
-        <li>天小天版权所有©</li>
-        <template v-if="host.includes('tianxiaotian')">
-            <li>
-                <a no-prefetch target="_blank" href="https://beian.miit.gov.cn">
-                    湘ICP备18008856号
-                </a>
-            </li>
-            <li>
-              <a
-                no-prefetch
-                target="_blank"
-                href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=43011102001726"
-              >
-                <img src="@/static/image/layout/备案图标.png" />
-                <span>湘公网安备 43011102001726号</span>
-              </a>
-            </li>
-        </template>
+      <li>天小天版权所有©</li>
+      <template v-if="host.includes('tianxiaotian')">
+        <li>
+          <a no-prefetch target="_blank" href="https://beian.miit.gov.cn">
+            湘ICP备18008856-1号
+          </a>
+        </li>
+        <li>
+          <a
+            no-prefetch
+            target="_blank"
+            href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=43011102001726"
+          >
+            <img src="@/static/image/layout/备案图标.png" />
+            <span>湘公网安备 43011102001726号</span>
+          </a>
+        </li>
+      </template>
+      <template v-else-if="host.includes('ttshouna')">
+        <li>
+          <a no-prefetch target="_blank" href="https://beian.miit.gov.cn">
+            湘ICP备18008856-2号
+          </a>
+        </li>
+      </template>
     </ul>
   </div>
 </template>
@@ -26,17 +33,17 @@
 <script>
 export default {
   name: "Copyright",
-    data(){
-        return {
-            host:''
-        }
-    },
-   
-    mounted() {
-       if (process.browser) {
-           this.host = location.host
-       }
-    },
+  data() {
+    return {
+      host: "",
+    };
+  },
+
+  mounted() {
+    if (process.browser) {
+      this.host = location.host;
+    }
+  },
 };
 </script>
 
